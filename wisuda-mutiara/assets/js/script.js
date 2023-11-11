2a93b3c2f13ad09512b29172da45d1c9f81fe799
fdjrr <fadjrir.co.id@gmail.com>
2023-11-11 22:13:48 +0700

    autoplay using javascript

--- wisuda-mutiara/assets/js/script.js
+++ wisuda-mutiara/assets/js/script.js
@@ -6,3 +6,10 @@ AOS.init({
 if (document.readyState == 'complete') {
   AOS.refresh();
 }
+
+var audio = new Audio(); // use the constructor in JavaScript, just easier that way
+audio.addEventListener("load", function () {
+  audio.play();
+}, true);
+audio.src = '../audio.mp3';
+audio.autoplay = true; // add this

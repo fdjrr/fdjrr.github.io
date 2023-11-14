fac6fecfb71f2325a3c68b73abe00d633ad7c525
fdjrr <fadjrir.co.id@gmail.com>
2023-11-14 14:12:21 +0700

    remove audio

--- wisuda-mutiara/assets/js/script.js
+++ wisuda-mutiara/assets/js/script.js
@@ -6,10 +6,3 @@ AOS.init({
 if (document.readyState == 'complete') {
   AOS.refresh();
 }
-
-var audio = new Audio(); // use the constructor in JavaScript, just easier that way
-audio.addEventListener("load", function () {
-  audio.play();
-}, true);
-audio.src = 'assets/audio.mp3';
-audio.autoplay = true; // add this
